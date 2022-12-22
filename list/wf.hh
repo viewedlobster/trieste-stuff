bd1c805b399326667258657fe7e9a684144ab0cf
Elias Castegren <elias@MacBook-Pro-25.local>
2022-12-22 13:55:45 +0100

    Finished well-formedness for list language

--- list/wf.hh
+++ list/wf.hh
@@ -6,6 +6,10 @@ namespace list
 {
   using namespace wf::ops;
 
+  inline constexpr auto wf_literal = Integer | String;
+
+  inline constexpr auto wf_parse_token = wf_literal | List;
+
   // A <<= B indicates that B is a child of A
   // ++ indicates that there are zero or more instances of the token
   inline constexpr auto wf_parser =
@@ -13,5 +17,6 @@ namespace list
     | (File <<= Group)
     | (List <<= (ListContents | Group))
     | (ListContents <<= Group++)
+    | (Group <<= wf_parse_token++)
     ;
 }
